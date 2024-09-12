d1415642b2a1104951935340ea023256188f0793
GatienBouyer <131701207+GatienBouyer@users.noreply.github.com>
2024-09-12 23:33:18 +0200

    Demo with htmx

--- src/interactive_graph/app.py
+++ src/interactive_graph/app.py
@@ -1,7 +1,8 @@
 from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import Response
-from starlette.routing import Route
+from starlette.routing import Mount, Route
+from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="src/templates")
@@ -13,6 +14,14 @@ def homepage(request: Request) -> Response:
     })
 
 
+def generate(request: Request) -> Response:
+    return templates.TemplateResponse(request, "generated.html", context={
+        "paragraph": "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    })
+
+
 app = Starlette(debug=True, routes=[
+    Mount('/static', StaticFiles(directory='src/static'), name='static'),
     Route('/', homepage),
+    Route('/generate', generate),
 ])
