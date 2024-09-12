be6f9ad9ee12d94801924fb66e0d35714033e284
GatienBouyer <131701207+GatienBouyer@users.noreply.github.com>
2024-09-12 23:16:43 +0200

    Demo with jinja

--- src/interactive_graph/app.py
+++ src/interactive_graph/app.py
@@ -1,11 +1,16 @@
 from starlette.applications import Starlette
 from starlette.requests import Request
-from starlette.responses import JSONResponse
+from starlette.responses import Response
 from starlette.routing import Route
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="src/templates")
 
 
-def homepage(_: Request) -> JSONResponse:
-    return JSONResponse({'hello': 'world'})
+def homepage(request: Request) -> Response:
+    return templates.TemplateResponse(request, "index.html", context={
+        "paragraph": "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    })
 
 
 app = Starlette(debug=True, routes=[
